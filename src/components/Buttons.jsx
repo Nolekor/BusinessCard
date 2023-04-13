552d6d32d4fc2f0c62b6d7ae5f2b4ace1ef35f5b
Nolekor <78369850+Nolekor@users.noreply.github.com>
2023-04-13 17:00:19 +0200

    add Buttons component

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Buttons = () => {
+  return (
+    <div className="btn-container">
+      <a href="https://www.facebook.com/NicoPelle27">
+        <button className="facebook">Facebook</button>
+      </a>
+      <a href="https://www.linkedin.com/in/nicola-pellegrino27/">
+        <button className="linkedin">LinkedIn</button>
+      </a>
+    </div>
+  );
+};
+
+export default Buttons;
