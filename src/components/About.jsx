543f308121bb1cfc6882e5acef309b79754ed916
Nolekor <78369850+Nolekor@users.noreply.github.com>
2023-04-13 17:00:28 +0200

    add About component

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <h3>About</h3>
+      <p>
+        Sono un Sviluppatore JS Junior. Ho acquisito solide competenze tecniche
+        grazie alla mia formazione e al lavoro su progetti personali sia per
+        quanto riguarda il lato frontend che per quanto riguarda il lato
+        backend, rispettivamente con i framework React.js e Express.
+      </p>
+      <h3>Interest</h3>
+      <p>
+        Tra i miei hobby preferiti ci sono i videogiochi, che mi appassionano
+        fin dall'infanzia, e le macchine, di cui apprezzo la tecnologia e
+        l'estetica. In particolare, mi piace utilizzare simulatori di guida per
+        sperimentare diverse esperienze di guida e per migliorare le mie
+        abilità. Ho sviluppato un interesse per la programmazione, che mi
+        consente di creare progetti e soluzioni innovative in diversi ambiti
+      </p>
+    </div>
+  );
+};
+
+export default About;
