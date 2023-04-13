71f2f4851e0bdffa4bbc2bf2255dd444c5ddbac1
Nolekor <78369850+Nolekor@users.noreply.github.com>
2023-04-13 17:00:51 +0200

    add components to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import Header from "./components/Header";
+import Buttons from "./components/Buttons";
+import About from "./components/About";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Buttons />
+      <About />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
